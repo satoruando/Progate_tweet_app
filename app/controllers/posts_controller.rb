fdbea02082c001be1dd0df8b50759877dd7cc1b5
satorusndo <ando.str15@gmail.com>
2022-03-07 04:52:16 +0000

    Ruby on Rails 5-4-5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,8 @@ class PostsController < ApplicationController
     redirect_to("/posts/index")
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'posts/:id' => 'posts#show'
   post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
 
   get "/" => "home#top"
   get "about" => "home#about"
