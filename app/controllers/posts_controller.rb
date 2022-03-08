db4ed6d44466404f004b1c3e082a3e3509c55697
satorusndo <ando.str15@gmail.com>
2022-03-08 06:12:11 +0000

    Ruby on Rails5-4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,4 +22,19 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+
+    redirect_to ("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    
+    redirect_to ("/posts/index")
+  end
+
 end
