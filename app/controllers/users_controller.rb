883223763c34c3519da3e2dc3986f4cd406daa8d
satorusndo <ando.str15@gmail.com>
2022-03-10 09:55:05 +0000

    Ruby on Rails 5-7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,21 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
+  
   def show
     @user = User.find_by(id: params[:id])
   end
-
+  
   def new
     @user = User.new
   end
   
-  def edit
-    @user = User.find_by(id: params[:id])
-  end
-
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default_user.jpg"
+    )
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
@@ -25,10 +25,21 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
   def update
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+    
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
       redirect_to("/users/#{@user.id}")
@@ -36,5 +47,5 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
-
+  
 end
