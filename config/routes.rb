1fb61bce942bfaa2f9e6ea3bb1e9c1d922624856
satorusndo <ando.str15@gmail.com>
2022-03-11 07:55:18 +0000

    Ruby onRails 5-8-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+
   post "users/:id/update" => "users#update"
-  
+
   get "users/:id/edit" => "users#edit"
   post "users/create" => "users#create"
   get "signup" => "users#new"
-  
+
   get 'users/index'
   get "users/:id" => "users#show"
 
